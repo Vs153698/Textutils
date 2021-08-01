b3f8552e14d47ad69d45252cd1e3d03da48f2333
Vaibhav Singh Bhadouria <86617405+Vs153698@users.noreply.github.com>
2021-08-01 15:28:58 +0530

    update settings.py
    hiding security key

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j&c+uew_ven^%ckyiinp^w^!(&dy5pu$ddr%&11-fn^a+6pn(n'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
